a03fa0037b5152180faa1b986b3f7ee23656a5f6
Emre Dokur <edokur@seven.com.au>
2024-04-29 12:12:22 +1000

    update test ar link

--- src/lib/ARFrame.svelte
+++ src/lib/ARFrame.svelte
@@ -2,5 +2,5 @@
   export let url: string;
 </script>
 <div class="h-full w-full bg-black fixed top-0 left-0">
-  <iframe class="h-hull w-full" src="https://playcanv.as/b/a22dceb7?tour=AR_Barn_Owl" frameborder="0" allowfullscreen/>
+  <iframe class="h-full w-full" src="https://dev.graviton.co/parramatta-ar/?tour=AR_Cricket" frameborder="0" allowfullscreen/>
 </div>
