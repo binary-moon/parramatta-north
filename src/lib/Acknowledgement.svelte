aad6e918d9f30ce57b9af6cf1cf87b4e6ed3533e
Emre Dokur <emre.dokur@gmail.com>
2024-09-26 15:04:00 +1000

    copy update

--- src/lib/Acknowledgement.svelte
+++ src/lib/Acknowledgement.svelte
@@ -10,6 +10,8 @@
   let imageLoaded = false; // Track image loading status
   const detailImagePlaceholder = 'https://placehold.co/2000x2000/black/333'
 
+  const audioUrl = null;
+
   const handleAudioClick = (event: MouseEvent) => {
     event.stopPropagation();
     console.log('Audio Click');
@@ -33,13 +35,14 @@
   >
     <div class="flex w-full h-full absolute top-0 left-0 bg-black bg-opacity-60">
       <div class="flex flex-col p-6 pt-48 gap-6">
+        {#if audioUrl}
         <button on:click={handleAudioClick} class="flex gap-2 items-center text-sm font-bold underline">
           <img src="/Audio_Description_ Icon.svg" alt="Audio"/> Listen to audio description
         </button>
+        {/if}
         <div class="relative flex flex-col gap-6 overflow-y-scroll pb-12">
-          <p>Beneath the silent echoes of wandering constellations, the fragmented whispers of forgotten rivers linger in the twilight. Spirals of laughter twist through the amber haze, vanishing before the horizon breaks into fragments of forgotten melodies. Shadows of neon birds stretch across the skyline, while velvet winds carry the dreams of machines lost to the ebbing tide. Flickering, shimmering, the stars breathe in rhythm with the murmurs of the earth, weaving a tapestry of tales left untold.</p>
-          <p>In the heart of the labyrinth, colors melt into one another, swirling beneath the weight of time’s unspoken riddles. The leaves, though transparent, whisper secrets to the translucent waves, while forgotten mirrors reflect the void. Paths once marked by invisible footprints now shimmer with a fleeting glow, vanishing as quickly as they appeared. Beneath it all, the hum of a nameless city pulses in sync with the static-filled silence, casting flickers of unrecalled moments across the endless sky.</p>
-          <p>The dance of unseen hands continues, tracing symbols upon the surface of forgotten seas. An orchestra of untuned winds hums through hollowed-out trees, filling the empty air with unformed words. Somewhere beyond the horizon, the unseen glow of shattered moons blurs the boundary between light and shadow, and yet, nothing remains of their echo. Silence falls, rises, and falls again, creating the illusion of motion where none exists, leaving only the faintest hint of memory in its wake.</p>
+          <p>The Macquarie Street East precinct is on Gadigal Land. We recognise that despite being changed with concrete, steel and bitumen, this Country is still cared for by the Gadigal people and their Ancestors continue to walk this land. We express our gratitude to Elders and Knowledge Holders whose wisdom, activism and knowledges have kept culture alive in this place.</p>
+          <p>We recognise our responsibility as the current caretakers of the Macquarie Street East Precinct to respect both this Country and those who belong to this Country. We strive to create spaces in the Macquarie Street East Precinct that are welcoming and safe for all, especially First Nations peoples whose cultures are valued as being deeply integral to this place.</p>
         </div>
         <div class="position: absolute bottom-0 left-0 w-full h-[150px]" style="background: linear-gradient(0deg, rgba(0,0,0,1) 30%, rgba(0,0,0,0) 100%);"></div>
         <Button buttonStyle="primary" additionalClasses="z-10" handleClick={handleContinue}>Continue</Button>
