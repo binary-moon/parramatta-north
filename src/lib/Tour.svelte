1b45d4f217ef4d57b4e6fec8ac969bd2be6d4470
Emre Dokur <emre.dokur@gmail.com>
2024-11-25 17:31:24 +1100

    zoom level re-adjusted

--- src/lib/Tour.svelte
+++ src/lib/Tour.svelte
@@ -209,7 +209,7 @@
     loadScript(`https://maps.googleapis.com/maps/api/js?key=${apiKey}`, async () => {
       map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
         center: defaultLocation,
-        zoom: 19,
+        zoom: 18,
         mapTypeControl: false,
         fullscreenControl: false,
         styles: [
