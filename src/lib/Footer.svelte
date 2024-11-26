d517b51a3eba224122b2af426efa83001337423e
Emre Dokur <emre.dokur@gmail.com>
2024-11-26 14:30:22 +1100

    Updated footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -13,7 +13,14 @@
     <span class="text-white text-[27px]/[38px]">{title}</span>
   </div>
   <div class="bg-accent p-6 pb-12">
-    <img src="/Footer_Logo.svg" alt="NSW Government - Property and Development NSW"/>
-    <p class="text-white border-t-[1px] border-neutral-content border-opacity-25 mt-6 pt-12">We pay respect to the Traditional Custodians and First Peoples of NSW, and acknowledge their continued connection to their country and culture.</p>
+    <!-- <img src="/Footer_Logo.svg" alt="NSW Government - Property and Development NSW"/> -->
+    <p class="text-white border-neutral-content border-opacity-25 mt-6">
+      {#if theme === "parramatta-north"}
+      Parramatta North. A place of connection and renewal – through reflection, discovery and opportunity. Here, a long-term vision is taking shape.
+      {/if}
+      {#if theme === "rgb"}
+      Macquarie Street East. Where Sydney’s rich Aboriginal and modern histories come to life and arts and culture thrive. Be Educated. Be Entertained. Be Surprised.
+      {/if}
+    </p>
   </div>
 </footer>
