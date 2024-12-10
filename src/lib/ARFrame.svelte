23ed4407af594f70d3f8af3edea281e90ba4eb1e
Emre Dokur <emre.dokur@gmail.com>
2024-12-10 16:23:59 +1100

    Add iframe permissions

--- src/lib/ARFrame.svelte
+++ src/lib/ARFrame.svelte
@@ -2,5 +2,5 @@
   export let url: string;
 </script>
 <div class="h-full w-full bg-black fixed top-0 left-0">
-  <iframe class="h-full w-full" src="https://dev.graviton.co/parramatta-ar/?tour=AR_Cricket" frameborder="0" allowfullscreen/>
+  <iframe class="h-full w-full" src="https://dev.graviton.co/parramatta-ar/?tour=AR_Cricket" frameborder="0" allow="camera;gyroscope;accelerometer;magnetometer;xr-spatial-tracking;microphone;" allowfullscreen/>
 </div>
