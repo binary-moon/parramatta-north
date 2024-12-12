43efb9dd6a0417c0cc48680c7bd6e750455f16b8
Emre Dokur <emre.dokur@gmail.com>
2024-12-12 20:04:46 +1100

    rotation update 2

--- src/lib/classes/UserMarker.ts
+++ src/lib/classes/UserMarker.ts
@@ -49,7 +49,7 @@ export class UserMarker extends google.maps.OverlayView {
 
   updateRotation(alpha: number) {
     if (this.div) {
-      this.div.style.transform = `rotate(${alpha}deg)`;
+      this.div.style.transform = `rotate(${alpha - 90}deg)`;
     }
   }
 
